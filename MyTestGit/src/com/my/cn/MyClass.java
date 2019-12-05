839b1be9897d5c045dcdbbaa26c55ce8cda37476
zhangsan <zhangsan@itcast.cn>
2019-12-05 16:22:16 +0800

    第二人不更新也修改后提交

--- MyTestGit/src/com/my/cn/MyClass.java
+++ MyTestGit/src/com/my/cn/MyClass.java
@@ -8,9 +8,14 @@ public class MyClass {
         System.out.println("第一次");
         System.out.println("第一次");
         System.out.println("添加点东西");
+        System.out.println("第二人不更新也添加点东西");
     }
 
     public void test(){
         System.out.println("第一次修改");
+    }
+
+    public void test3(){
+        System.out.println("第二人不更新第二次修改");
     }
 }

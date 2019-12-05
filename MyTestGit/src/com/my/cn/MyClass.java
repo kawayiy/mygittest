5d6627dc18de51f0994e038178cb83e5fb475ea6
zhangsan <zhangsan@itcast.cn>
2019-12-05 16:08:59 +0800

    MyClass第一次提交

---
+++ MyTestGit/src/com/my/cn/MyClass.java
@@ -0,0 +1,11 @@
+package com.my.cn;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("第一次");
+        System.out.println("第一次");
+        System.out.println("第一次");
+        System.out.println("第一次");
+    }
+}
